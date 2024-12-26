036531079179315e19c1262ef00a1891306bc557
flizcanor <flizcanor@icloud.com>
2024-12-26 09:24:02 -0500

    avances

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,12 +22,8 @@
             .content-wrapper.px-lg-4.pt-lg-4.px-xxl-5.py-xxl-5.text-start
               p.home-main-title UNIDAD {{ `${globalData.numeroUnidad}` }}
               h1.home-title.mb-4 {{ globalData.tituloUnidad }}
-              p.descripcion.mb-4 Desde los comienzos de la humanidad, la relación entre los principios éticos, el desarrollo económico y la sostenibilidad ha sido fundamental para garantizar un progreso equilibrado y equitativo. En un contexto globalizado, donde los desafíos éticos y ambientales son cada vez más evidentes, la bioética emerge como una herramienta clave para orientar decisiones económicas responsables que respeten tanto el medio ambiente como los derechos humanos.
-
-              p.descripcion.mb-4 En esta unidad, profundizaremos en el rol de la bioética como pilar de la sostenibilidad en el desarrollo económico, explorando temas esenciales como la aplicación de principios éticos al uso de recursos naturales, la influencia de las políticas globales en la protección de los derechos humanos y la ética en la innovación tecnológica. 
+              p.descripcion.mb-4 La propuesta de proyecto transforma ideas en planes viables mediante análisis de necesidades, viabilidad, planificación y gestión de recursos. El ejemplo "SanoDelight" aborda la demanda de panadería saludable con estrategias como tiendas físicas, en línea y sostenibilidad. Los componentes clave incluyen problema, objetivos, mercado y estrategias. La evaluación de impacto utiliza indicadores como VAN, TIR y relación costo-beneficio. Los criterios de eficiencia y equidad aseguran uso óptimo de recursos, máxima rentabilidad y distribución justa de beneficios con inclusión social y sostenibilidad.
               
-              p #[b Prepárate para explorar los retos y oportunidades que la sostenibilidad y la bioética presentan en un mundo en constante evolución.]
-
               .button-container.mt-auto
                 router-link.btn-iniciar.w-100.w-md-auto(:to="{ name: 'introduccion' }")
                   .iniciar-btn-container.w-100.w-md-auto
